7041b046079d5080ea1165a32f47eed13ddd1356
Javier Bonilla <bjavier@umich.edu>
2024-06-24 16:29:53 -0400

    updated my plugin so it can be used by other people who try to install it

--- main.ts
+++ main.ts
@@ -4,7 +4,6 @@ import * as path from 'path';
 import * as os from 'os';
 import axios from 'axios';
 import { config } from 'config';
-// Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -25,87 +24,54 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 		let gptfilePath: string;
 		const homeDir = os.homedir();
+
 		let crntYear: number = new Date().getFullYear();
 		let crntMonth: number = new Date().getMonth() + 1;
-		//Will be trying to implemet a folder system to store gpt logs
-		const crntFilePath = process.cwd();
-		//new Notice(crntFilePath);
-		//const filePath1 = path.join(crntFilePath, `GPT-Logs/${crntYear}/2024-${crntMonth}`)
-		// if (!fs.existsSync(crntFilePath)) {
-		// 	// If it doesn't exist, create it
-		// 	fs.mkdirSync(crntFilePath, {recursive: true});
-		// }
+
 		const vault_name = this.app.vault.adapter.getName();
-		//new Notice(fpth);
-		const filePath1 = path.join(homeDir, `Documents/2024/summer_intern/${vault_name}`);
-		//const filePath1 = path.join(homeDir, `Documents/GPT-Logs/${crntYear}/2024-${crntMonth}`);
-		if (!fs.existsSync(filePath1)) {
+		const pathToVault = config.location;
+
+		const filePath1 = path.join(homeDir, `${pathToVault}/${vault_name}`);
+		
+		if (!fs.existsSync(filePath1)) {// creates path to vault if it doesn't exist
 			// If it doesn't exist, create it
 			fs.mkdirSync(filePath1, {recursive: true});
 		}
-		const filePath2 = path.join(filePath1,`GPT-Logs/${crntYear}/2024-${crntMonth}` )
-		if (!fs.existsSync(filePath2)) {
+		const filePath2 = path.join(filePath1,`${config.gpt_log}/${crntYear}/2024-${crntMonth}` )
+		
+		if (!fs.existsSync(filePath2)) {// creates folders for year and month
 			// If it doesn't exist, create it
-			//this.app.vault.adapter.mkdir(`${crntYear}`);
 			fs.mkdirSync(filePath2, {recursive: true});
 		}
 		
-		//const filePath1 = path.join(homeDir, `Documents/2024/summer_intern/Obsidian-cligpt`);
-		function generateTimestampedFileName(): string { //generates the filename based on timestamp
+		function generateTimestampedFileName(): string { //generates the filename based on date and time
 			const timestamp = new Date().toISOString();
 			return `cligpt-${timestamp}.md`;
 		}
 		function createAndWriteToFile(directory: string): string {//creates file using generated timestamp, and writes our info into file
 			const fileName = generateTimestampedFileName();
 			const filePath = path.join(directory, fileName);
-			const message = "role: You are an expert Software Developer\nmodel: gpt-4\n## Question:\n---------\n"
+			const message = `role: ${config.role}\nmodel: ${config.model}\n## Question:\n---------\n`
 		
 			try {
 				fs.writeFileSync(filePath, message, { flag: 'a' });
-				new Notice(`File created and content written: ${fileName}`);
-				//vscode.window.showInformationMessage(`File created and content written: ${fileName}`);
 			} catch (error) {
 				new Notice('Failed to create and write to the file');
-				//vscode.window.showErrorMessage('Failed to create and write to the file');
 			}
-			//return fileName;
 			return filePath;
 		}
-		// async function openFileInVSCode(filePath: string) {
-		// 	let fileToOpen = app.vault.getAbstractFileByPath(filePath);
-		// 	if(fileToOpen) {
-		// 		app.workspace.activeLeaf?.open(fileToOpen);
-		// 		let newLeaf = app.workspace.splitActiveLeaf(); 
-    	// 		newLeaf.open(fileToOpen);
-		// 	// Open the file in a new leaf
-		// 		//app.workspace.openLeafInNewWindow(fileToOpen);
-		// 	}
-		// }
 
-		async function sendToChatGPT(content: string): Promise<string | null> {
-			//return content;
+		async function sendToChatGPT(content: string): Promise<string | null> {//sends content to chatGPT using info from config.ts file
 			const apiKey: string = config.apiKey;
 			const model: string = config.model;
-			//dotenv.config();
-			//const apiKey: string | undefined = process.env.API_KEY;
-			if(apiKey){
-				new Notice(apiKey);
-			}
-			else{
-				new Notice("undefined apikey");
-			}
-			
-			//const model: string | undefined = process.env.MODEL;
+
 			if(model){
 				new Notice(model);
 			}
 			else{
 				new Notice("undefined model");
 			}
-			//const apiKey = 'REPLACE WITH API KEY';//process.env.OPENAI_API_KEY; 
 			let auth: string = 'Bearer ' + apiKey;
-			// const api_key = process.env.OPENAI_APIKEY;
-			// vscode.window.showInformationMessage(api_key);
 			try {
 				const response = await axios.post('https://api.openai.com/v1/chat/completions', {
 					model: model, // Specify the correct model
@@ -119,8 +85,7 @@ export default class MyPlugin extends Plugin {
 					}
 				});
 				if (response.data && response.data.choices && response.data.choices.length > 0) {
-					//return response.request.choices[0].message.content;
-					let final:string = response.data.choices[0].message.content;//.data.choices[0].text;//.data.choices[0].text.trim();
+					let final:string = response.data.choices[0].message.content;
 					return final;
 				}
 				else{
@@ -141,12 +106,11 @@ export default class MyPlugin extends Plugin {
 			return null;
 		}
 
-		//
 		this.addRibbonIcon('dog', 'cligpt', () => {
 			gptfilePath = createAndWriteToFile(filePath2);
-			//openFileInObsidian(gptfilePath);
 		});
-		this.addCommand({
+
+		this.addCommand({//command for sending current open file to gpt
 			id: 'send-to-gpt',
 			name: 'send to gpt',
 			editorCallback: async () => {
@@ -156,20 +120,13 @@ export default class MyPlugin extends Plugin {
 				if (activeLeaf) {
 					let view = activeLeaf.view
 					if (view instanceof MarkdownView) {
-						//if(!gptfilePath){
 						let activeFile = view.file;
-						//new Notice(gptfilePath);
 						if (activeFile) {
-							// basename gives you the filename (with extension)
 							gptfilePath = path.join(filePath2, activeFile.basename) + ".md";
-							//console.log(fileName);
-							//new Notice(gptfilePath);
 						}
 						else{
 							new Notice("FAIL");
 						}
-						//
-						//}
 						let editor = view.editor
 						let doc = editor.getDoc()
 						let fileContent = doc.getValue() // This is the text content of your current file
@@ -199,20 +156,13 @@ export default class MyPlugin extends Plugin {
 				if (activeLeaf) {
 					let view = activeLeaf.view
 					if (view instanceof MarkdownView) {
-						//if(!gptfilePath){
 						let activeFile = view.file;
-						//new Notice(gptfilePath);
 						if (activeFile) {
-							// basename gives you the filename (with extension)
 							gptfilePath = path.join(filePath2, activeFile.basename) + ".md";
-							//console.log(fileName);
-							//new Notice(gptfilePath);
 						}
 						else{
 							new Notice("FAIL");
 						}
-						//
-						//}
 						let editor = view.editor
 						let doc = editor.getDoc()
 						const lineCount = editor.lineCount();
@@ -241,67 +191,6 @@ export default class MyPlugin extends Plugin {
 			}
 		})
 
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -314,47 +203,5 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
